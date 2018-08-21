14c2ed3b0188fc75b13e5257e22dc6f878701d25
Lucas Souto <lssouto96@gmail.com>
2018-08-21 11:24:30 +0000

    loader ok

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,30 @@
   <div id="app">
     <!--<img alt="Vue logo" src="./assets/logo.png">-->
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <loader :show="loader"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import Loader from './components/Loader.vue';
 
 export default {
   name: 'app',
+  data() {
+    return {
+      loader: false,
+    };
+  },
   components: {
     HelloWorld,
+    Loader,
+  },
+  mounted() {
+    const vm = this;
+    setTimeout(() => {
+      vm.loader = true;
+    }, 2000);
   },
 };
 </script>
@@ -24,5 +38,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  position: relative;
 }
 </style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="pre-load">
+        <div class="pre-load-background">
+            <div class="pre-load-main">
+                <span class="pre-load-dot">.</span>
+                <span class="pre-load-dot">.</span>
+                <span class="pre-load-dot">.</span>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'loader',
+    data() {
+        return {
+            el: null,
+        };
+    },
+    mounted() {
+        this.el = document.getElementById('pre-load');
+        this.hideLoader();
+    },
+    methods: {
+        showLoader() {
+            const vm = this;
+            this.el.classList.remove('hide');
+            setTimeout(() => {
+                vm.el.classList.add('enter');
+                vm.el.classList.remove('leave');
+            }, 500);
+        },
+        hideLoader() {
+            const vm = this;
+            setTimeout(() => {
+                vm.el.classList.add('leave');
+            }, 500);
+            setTimeout(() => {
+                vm.el.classList.add('hide');
+            }, 1250);
+        },
+    },
+    props: [
+        'show',
+    ],
+    watch: {
+        show(value) {
+            if (value) {
+                this.showLoader();
+            } else {
+                this.hideLoader();
+            }
+        },
+    },
+};
+</script>
+
