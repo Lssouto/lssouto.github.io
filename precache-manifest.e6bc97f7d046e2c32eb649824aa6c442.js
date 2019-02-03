25a298522322575f228032409f0781be89d4c07c
Lucas Souto <lssouto96@gmail.com>
2019-02-03 18:40:41 +0000

    compiled website

---
+++ precache-manifest.e6bc97f7d046e2c32eb649824aa6c442.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c52602beaa67d765a37dcde72ee5ccc",
+    "url": "/img/portifolio-trabalhar-nos-eua-2.9c52602b.png"
+  },
+  {
+    "revision": "07e1ab36d86f09582014f6a1cd81ed25",
+    "url": "/img/owl-logo.svg"
+  },
+  {
+    "revision": "47776ec8a8ae43d94a48",
+    "url": "/js/chunk-vendors.265fc9c6.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "64e5a5092a97d8f5b6fbd74a76075ffe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38c613da654864a88bcbda6268f45529",
+    "url": "/img/portifolio-diario-oficial-2.38c613da.png"
+  },
+  {
+    "revision": "7d231d7a686ff253258bc34cccff16c0",
+    "url": "/img/portifolio-tec-gelo.7d231d7a.png"
+  },
+  {
+    "revision": "a8c4aef97b06989e812e",
+    "url": "/js/app.19ba73a0.js"
+  },
+  {
+    "revision": "a1e1eebf4ef35fa8f7f249c720e4ea17",
+    "url": "/img/owl-logo-blink.a1e1eebf.png"
+  },
+  {
+    "revision": "1369452cf8398f30f901ca88ddd4cf19",
+    "url": "/img/icomoon.1369452c.svg"
+  },
+  {
+    "revision": "02ddf50d56e6b2ead9ed627c14a35791",
+    "url": "/img/gulp.02ddf50d.png"
+  },
+  {
+    "revision": "1c78ff4c4c8ad09e95945dac9ff00577",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "a8c4aef97b06989e812e",
+    "url": "/css/app.adafc2e1.css"
+  },
+  {
+    "revision": "1c78ff4c4c8ad09e95945dac9ff00577",
+    "url": "/apple-touch-icon.png"
+  }
+];
